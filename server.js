6aa8efb625c468951cea6fd56da3f21c9d978b5f
kdingman <kcdonlan@yahoo.com>
2022-04-07 15:25:00 -0400

    server created

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/unstructured-network', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+app.use(require('./routes'));
+
+mongoose.set('debug', true);
+
+app.listen(PORT, () => console.log(`🪄 Connected on localhost:${PORT}`));
